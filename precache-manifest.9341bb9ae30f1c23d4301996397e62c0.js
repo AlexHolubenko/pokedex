0e339b3693792706eb14ba33dc3acc27144323b0
Oleksandr Holubenko <naturlichgood@gmail.com>
2020-05-05 09:42:07 +0300

    Updates

---
+++ precache-manifest.9341bb9ae30f1c23d4301996397e62c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46d268019cabad2b4ad18da38900d08d",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "0bc8a708a3ab95bf2510",
+    "url": "/pokedex/static/css/main.66b5369f.chunk.css"
+  },
+  {
+    "revision": "abacdb9e9e6fd4d96614",
+    "url": "/pokedex/static/js/2.908d6153.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokedex/static/js/2.908d6153.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bc8a708a3ab95bf2510",
+    "url": "/pokedex/static/js/main.a2fa2a12.chunk.js"
+  },
+  {
+    "revision": "014345e0bd0e463ebfd2",
+    "url": "/pokedex/static/js/runtime-main.83eb840c.js"
+  }
+]);
